a58d9f16affc80bedc86db5f1e2d2263d884179d
DRMINA2019 <drmina2007@yahoo.com>
2026-06-07 13:29:17 +0300

    new error corrected

--- app/ui/calcDashboard/issuers/create-issuer-form.tsx
+++ app/ui/calcDashboard/issuers/create-issuer-form.tsx
@@ -53,7 +53,7 @@ export default function Form({ action }: FormProps) {
                 id="name"
                 name="name"
                 type="text"
-                placeholder="Enter Issuer Name"
+                placeholder="Enter Company Name"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               {/* <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
@@ -71,7 +71,7 @@ export default function Form({ action }: FormProps) {
                 id="email"
                 name="email"
                 type="text"
-                placeholder="Enter Issuer Email"
+                placeholder="Enter Company Email"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               {/* <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
